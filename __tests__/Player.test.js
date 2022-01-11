08e261578713378a66f916f7d2e356eeef859d62
CKURZ <ckurz098@gmail.com>
2022-01-11 12:22:09 -0800

    added player methods

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -3,8 +3,8 @@ const Player = require('../lib/Player');
 const Potion = require('../lib/Potion');
 
 jest.mock('../lib/Potion');
-
-test('creates a player object with name,health,strength, agility', () => {
+//test create player object
+test('creates a player object', () => {
     const player = new Player('Dave');
     console.log(player.inventory);
 
@@ -15,4 +15,24 @@ test('creates a player object with name,health,strength, agility', () => {
     expect(player.inventory).toEqual(
         expect.arrayContaining([expect.any(Object)])
     );
+
 });
+//test getStats
+test("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+  
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+  });
+//test getInventory
+  test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+  
+    expect(player.getInventory()).toEqual(expect.any(Array));
+  
+    player.inventory = [];
+  
+    expect(player.getInventory()).toEqual(false);
+  });
